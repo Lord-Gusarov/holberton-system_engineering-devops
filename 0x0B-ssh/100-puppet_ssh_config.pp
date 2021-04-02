a54802d26580ce3ba2080902bc5e165d87e3010a
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-02 18:46:01 +0000

    Ads task #100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# Makes sure 2 setting are in the client SSH configuration file
+$path_conf = '/etc/ssh/ssh_config'
+
+file { $path_conf:
+  ensure => present,
+}
+file_line { 'Turn off passwd auth':
+  path    => $path_conf,
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
+  require => File[$path_conf]
+}
+file_line { 'Declare identity file':
+  path    => $path_conf,
+  line    => 'IdentityFile ~/.ssh/holberton',
+  match   => '^IdentityFile',
+  require => File[$path_conf],
+}
