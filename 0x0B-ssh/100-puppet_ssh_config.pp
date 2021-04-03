8aa71481a0f681d92d92b920000df5d2f45a9077
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-03 14:30:21 +0000

    Adds 4 spaces before the lines insert to mantain format

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,13 +6,13 @@ file { $path_conf:
 }
 file_line { 'Turn off passwd auth':
   path    => $path_conf,
-  line    => 'PasswordAuthentication no',
+  line    => '    PasswordAuthentication no',
   match   => '^\s*PasswordAuthentication',
   require => File[$path_conf]
 }
 file_line { 'Declare identity file':
   path    => $path_conf,
-  line    => 'IdentityFile ~/.ssh/holberton',
+  line    => '    IdentityFile ~/.ssh/holberton',
   match   => '^\s*IdentityFile',
   require => File[$path_conf],
 }
