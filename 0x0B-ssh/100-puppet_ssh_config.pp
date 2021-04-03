89f3ed6d9fde1f78fb533233c53b3e4c83fd8e7b
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-03 14:35:27 +0000

    Remove use of variable for the path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 # Makes sure 2 setting are in the client SSH configuration file
-$path_conf = '/etc/ssh/ssh_config'
 file_line { 'Turn off passwd auth':
-  path    => $path_conf,
-  line    => '    PasswordAuthentication no',
-  match   => '^\s*PasswordAuthentication',
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^\s*PasswordAuthentication',
 }
 file_line { 'Declare identity file':
-  path    => $path_conf,
-  line    => '    IdentityFile ~/.ssh/holberton',
-  match   => '^\s*IdentityFile',
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/holberton',
+  match => '^\s*IdentityFile',
 }
