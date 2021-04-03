8b0ba2baf92a0f9e5cdcb24e515da8afb0ef6d78
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-03 17:23:43 +0000

    Keeping it simple, no vars, no additional checks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,9 @@
 # Makes sure 2 setting are in the client SSH configuration file
-$path_conf = '/etc/ssh/ssh_config'
-
-file { $path_conf:
-  ensure => file,
-}
 file_line { 'Turn off passwd auth':
-  path    => $path_conf,
-  line    => 'PasswordAuthentication no',
-  match   => '^\s*PasswordAuthentication',
-  require => File[$path_conf],
+  path => '/etc/ssh/ssh_config'
+  line => '    IdentityFile ~/.ssh/holberton'
 }
 file_line { 'Declare identity file':
-  path    => $path_conf,
-  line    => 'IdentityFile ~/.ssh/holberton',
-  match   => '^\s*IdentityFile',
-  require => File[$path_conf],
+  path => '/etc/ssh/ssh_config'
+  line => '    PasswordAuthentication no'
 }
