ea71fa0c4317b096b6f9f0cf1f365fb6f371b6ad
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-03 22:21:01 +0000

    Adds task #1 --Pyct 0x15

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Gather data from an API and export in CVS format"""
+import requests
+from sys import argv, exit
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("Usage: {} <employeeID>".format(argv[0]))
+        exit()
+
+    userId = argv[1]
+    site = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(site + 'users?id={}'.format(userId)).json()
+    if not user:
+        print("No employee record found for ID: {}".format(userId))
+        exit()
+    username = user[0].get('username')
+    to_dos = requests.get(site + 'todos?userId={}'.format(userId)).json()
+    done = [t['title'] for t in to_dos if t['completed'] is True]
+
+    with open('{}.cvs'.format(userId), 'w') as out:
+        for task in to_dos:
+            out.write('"{}","{}","{}","{}"\n'.format(userId, username,
+                                                     task['completed'],
+                                                     task['title']))
