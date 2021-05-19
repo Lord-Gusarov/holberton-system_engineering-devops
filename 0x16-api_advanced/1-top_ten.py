d96d1ec718bc1f48335034471f6f9691863c1c26
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-05-19 14:17:31 +0000

    Limits the returned result to 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 
 def top_ten(subreddit):
     """ Fetches title of the top ten posts"""
-    site = 'https://www.reddit.com/r/{}/top.json'.format(subreddit)
+    site = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
     head = {'User-Agent': 'My Super Agent'}
     response = requests.get(site, headers=head)
     try:
@@ -14,7 +14,7 @@ def top_ten(subreddit):
             raise Exception('Invalid subreddit or a Redirection')
         else:
             posts = response.json()['data']['children']
-            for idx in range(10):
-                print(posts[idx]['data']['title'])
+            for post in posts:
+                print(post['data']['title'])
     except:
         print('None')
